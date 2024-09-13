04c2b9edae0cecc65ac23ca657d5df1d08c3122b
micheal-olatunbosun <molatunbosun7@gmail.com>
2024-09-13 22:29:38 +0100

    render push

--- render-build.sh
+++ render-build.sh
@@ -3,4 +3,5 @@ apt-get update && apt-get install -y \
 libsm6 libxext6 libxrender-dev \
 libopencv-dev python3-opencv \
 build-essential cmake \
+&& pip install --upgrade pip \
 && pip install -r requirements.txt
